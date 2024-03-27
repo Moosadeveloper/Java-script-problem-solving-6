fccf3b87760fcd1150415e1b7a0d3982859dd928
Moosadeveloper <janmoosa77@gmail.com>
2024-03-27 19:43:26 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+
+
+x =100;
+x--;
+// Answer is 99
+
+
+
+var x = 50;
+var y = x++;
+// answer is 50
+
+
+var y = 50;
+var z = --y;
+// answer is 49
+
+
+
+var newNum = num--;
+
+
+
+var num = 1;
+
+
+
+var x=12;
+x++;
+alert(x);
+
+
+
+var x=19;
+x++;
+alert(x);
+
+
+
+
+
+
+
+
+
